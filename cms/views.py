369f6f9d10209a6aaa8271f972a38ffd72d8244c
Nina Pavlich <nina@ninalp.com>
2018-05-09 20:13:43 -0700

    fix to custom error views

--- cms/views.py
+++ cms/views.py
@@ -41,14 +41,14 @@ class GUITemplateView(UserPassesTestMixin, TemplateView):
         return context
 
 
-view_400(request, exception, template_name='404.html'):
+def view_400(request, exception, template_name='404.html'):
     pass
 
-view_403(request, exception, template_name='404.html'):
+def view_403(request, exception, template_name='404.html'):
     pass
 
-view_404(request, exception, template_name='404.html'):
+def view_404(request, exception, template_name='404.html'):
     pass
 
-view_500(request, template_name='404.html'):
+def view_500(request, template_name='404.html'):
     pass    
