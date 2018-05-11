67a51234328f6d3b1d4059952040975ed8d8891b
Nina Pavlich <nina@ninalp.com>
2018-05-11 00:30:20 -0700

    add pagination to rest framework by deafult

--- cms/settings.py
+++ cms/settings.py
@@ -205,7 +205,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
 
 MQTT_USERNAME = env("MQTT_USERNAME", default=None)
